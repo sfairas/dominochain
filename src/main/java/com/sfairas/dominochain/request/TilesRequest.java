a783bce4baa9b154129282bec942d52a667c27d6
Dimitrios <sfairas@gmail.com>
2022-02-19 17:31:39 +0000

    Cleaning up

--- src/main/java/com/sfairas/dominochain/request/TilesRequest.java
+++ src/main/java/com/sfairas/dominochain/request/TilesRequest.java
@@ -3,6 +3,7 @@ package com.sfairas.dominochain.request;
 import java.util.List;
 
 import com.sfairas.dominochain.model.Tile;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/sfairas/dominochain/service/ChainEngine.java
+++ src/main/java/com/sfairas/dominochain/service/ChainEngine.java
@@ -1,7 +1,6 @@
 package com.sfairas.dominochain.service;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
